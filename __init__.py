f606196014c805facdda83a9fa770027ee33d686
peter <peter@hikerlink.org>
2014-03-21 14:56:18 +0800

    passenger on board

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = 'peter'
+__version__ = '0.1'
+
+from bottle import Bottle
+app = Bottle()
+from controllers import *
---
+++ passenger_wsgi.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'peter'
+
+import os
+import sys
+
+INTERP = "/home/peter/projects/tardis/bin/python"
+#INTERP is present twice so that the new python interpreter knows the actual executable path
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+nginx_configuration = os.path.dirname(__file__)
+project = os.path.dirname(nginx_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+
+from . import app
+
+def application(environment, response):
+  return app.wsgi(environment, response)
