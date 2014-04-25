f85cbc1869586b11c4316790cb57d21b6723cf77
peter <peter@hikerlink.org>
2014-04-26 00:49:38 +0800

    disable xsrf for api

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 define("port", default=8888, help="run on the given port", type=int)
 define("config", default=None, help="tornado config file")
-define("debug", default=False, help="debug mode")
+define("debug", default=True, help="debug mode")
 tornado.options.parse_command_line()
 
 MEDIA_ROOT = path(ROOT, 'media')
